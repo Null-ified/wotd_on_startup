b6d969bac20e79f39ab11d16d21ab09ae88757cb
Null-ified <63539836+Null-ified@users.noreply.github.com>
2024-11-04 02:15:22 -0800

    Pushing for desktop

--- Webster.py
+++ Webster.py
@@ -3,29 +3,65 @@ import requests
 import re 
 import main
 
+'''
+Goal for fetching
+    Grab WOTD via big font
+    Def is found right before example, so have soup find //'s tag first then have it look up child coming before it
+    Pattern for example is that inside of wod def container, the words after the // is the example.     
+    have soup find tags that have // as element and parse that tags contents
+
+'''
+
 def webster_WOTD(r_obj:requests.Response):
     ''' Soup to decode -> Reg Expression + Soup to find attributes with WOTD
-        -> Jump to proper paper to extract rest of the info.'''    
+        -> Jump to proper page to extract rest of the info.'''    
     web_Soup = BeautifulSoup(r_obj.text, "html5lib")
 
-    # Filter for div tag with class attribute that has wotd and look at its children's element
-    print("[DEBUG]: Printing out matched tags for word def")
-    for soup in web_Soup.find_all("div", class_=re.compile('(wotd|word of the day|definition)') ):
-        print(soup['class'])    
+    print("[DEBUG]: searching for better def and making list of examples")
+    def wotd_def():
+        for soup in web_Soup.find_all("div", class_=re.compile('(wod-definition-container)')):
+            return soup.p.text
+
+
     
-    #wotd   
-    print("[DEBUG]: Printing out matched tags for word name + other details")
+    #Debug finding potential container with wotd info   
+    ''' print("[DEBUG]: Printing out matched tags for word name + other details")
     for soup in web_Soup.find_all("div", class_=re.compile('(word)') ):
-        print(soup['class'])  
+        print(soup['class'])  '''
 
-    print("[DEBUG]: MORE DEBUGGING")
-    for soup in web_Soup.find_all("div", class_=re.compile('(wotd|word of the day|definition)') ):
+    '''print("[DEBUG]: MORE DEBUGGING")
+    for soup in web_Soup.find_all("div", class_=re.compile('(wotd|wod|word of the day|definition)') ):
         print(soup.p)
+    '''
 
+    #Find all available examples
+    print("Finding all examples starting with // via text")
 
+    # hey = web_Soup.find_all(string=re.compile(r'^//(.*)', re.DOTALL))
+    ''' for soup in hey:
+        print(soup)'''
+    def wotd_examples():
+        #Need to only return the examples man
+        for soup in web_Soup.find_all("p"):
+            if soup.text and re.match(r'^\s*//', soup.text):
+                print(soup.text)
+        return soup.text
+    
+
+# Rewrite with find 
     main.wotd_info(name = web_Soup.find_all("h2", class_=re.compile('(word)'))[0].string,
                  word_type = web_Soup.find_all("div", class_=re.compile('(word)'))[2].contents[1].string,
                  syllabes = web_Soup.find_all("span", class_=re.compile('(word)'))[0].string,
-                 definition = web_Soup.find_all("div", class_=re.compile('(wotd|word of the day|definition)'))[0].p.string,
-                 example = web_Soup.find_all("div", class_=re.compile('(wotd|word of the day|definition)') )[0].p.find_next("p"),
-)
+                 definition = wotd_def(),
+                 example = wotd_examples())
+    
+
+
+''' #valid
+  main.wotd_info(name = web_Soup.find_all("h2", class_=re.compile('(word)'))[0].string,
+                 word_type = web_Soup.find_all("div", class_=re.compile('(word)'))[2].contents[1].string,
+                 syllabes = web_Soup.find_all("span", class_=re.compile('(word)'))[0].string,
+                 definition = main.cull_tags(web_Soup.find("div", class_=re.compile('(wotd|word of the day|definition)')).p),
+                 example = web_Soup.find("p", text=re.compile(r'^\s*//') ))
+
+'''
--- main.py
+++ main.py
@@ -2,48 +2,84 @@ from bs4 import BeautifulSoup
 import re
 import requests
 import Webster
-#All of this will get refactored eventually I swear
-#need to solve circular import
 
-#Printing out choices and error checking
+
+''' 
+To-Do
+
+Solve
+    Circular import
+    Printing out choices and error checking
+
+Feature
+    Allowing someone to check the word of the days for any day of any year
+        Using their word of the day Calender 
+    OR
+    Make a random function where you hit random word and it will fetch a random word for you.
+    
+    Click on a word and hyperlinks to webster dictionary
+
+'''
+
 def Choices():
     print("Please select one of the following sites to know the word of the day of:")
     print("[] Webster Dictionary | A ")
     print("[] Oxford Dictionary | B ")
     print("[] Dictionary.com | C ")
+    print("DEBUG: Testing failed requests | F")
     
-    valid_choices = {'A', 'B', 'C'}
+    valid_choices = {'A', 'B', 'C', 'F'}
 
     while True:
         choice = input("Enter letter corresponding to site: ").strip().upper()
         
         if choice in valid_choices:
             print(f"You selected option {choice}.")
-            return choice  # Or call a function based on the choice
+            return choice  
         else:
-            print("Invalid input. Please enter either A, B, or C.")
+            print("Invalid input. Please enter either A, B, C, F")
 
-       
+
+def request_connection_Execptions(x:str):
+    '''Simple try catch when making connections. Might show more execeptions later'''
+    try:
+        ok_requestObj = requests.get(x, timeout=5)
+        return ok_requestObj
+    except requests.exceptions.RequestException as e:
+        raise Exception(e) 
+    
+
 def siteResponseObjGenerator(choice:chr):
-    match choice:
-        case 'A': 
-            print("[x] Webster Dictionary | A ")
-            r_webster = requests.get("https://www.merriam-webster.com/word-of-the-day")
-            return r_webster, "Webster"
-        case 'B': 
-            print("[x] Oxford Dictionary")
-            r_Oxford = requests.get("https://www.oed.com/")
-            return r_Oxford, "Oxford"
-        case 'C':
-            print("[x] Dictionary.com | C ")
-            r_Dictionary = requests.get("https://www.dictionary.com/e/word-of-the-day/")
-            return r_Dictionary, "Dictionary"
+        match choice:
+            case 'A': 
+                print("[x] Webster Dictionary | A\n")
+                print("Creating Webster Object...")
+                r_webster = request_connection_Execptions("https://www.merriam-webster.com/word-of-the-day")
+                return r_webster, "Webster"
+            case 'B': 
+                print("[x] Oxford Dictionary")
+                print("Creating Oxford Object...")
+                r_Oxford = request_connection_Execptions("https://www.oed.com/")
+                return r_Oxford, "Oxford"
+            case 'C':
+                print("[x] Dictionary.com | C ")
+                print("Creating Dictionary  Object...")
+                r_Dictionary = request_connection_Execptions("https://www.dictionary.com/e/word-of-the-day/")
+                return r_Dictionary, "Dictionary"
+            case 'F':
+                print("[DEBUG] Testing connection exceptions...")
+                r_Debug_F = request_connection_Execptions("lol")
+                return r_Debug_F, "This should output an error"
 
+#This is completely unneeded because the .text method
+'''
 def cull_tags(soup_string: str):
     # Extracting text w/o tags
     clean_text = soup_string.get_text(separator=' ',strip=True)
     clean_text = re.sub(r'//', '', clean_text)
-    return clean_text        
+    return clean_text.strip()     
+'''
+
 
 def wotd_info(name:BeautifulSoup, word_type:BeautifulSoup, syllabes:BeautifulSoup, definition:BeautifulSoup,
               example:BeautifulSoup):
@@ -59,7 +95,7 @@ def wotd_info(name:BeautifulSoup, word_type:BeautifulSoup, syllabes:BeautifulSou
           Word type: {wotd_info['word type']}
           Syllabes: {wotd_info['syllabes']} 
           Definition: {wotd_info['definition']}
-          Example: {cull_tags(wotd_info['example'])}
+          Example: {(wotd_info['example'])}
           """
           )
 
@@ -69,17 +105,20 @@ def wotd_info(name:BeautifulSoup, word_type:BeautifulSoup, syllabes:BeautifulSou
 def main(): 
     #rObj_ChosenSite[1](Tuple) for specific site 
     rObj_ChosenSite = siteResponseObjGenerator(Choices())
+    
 
     match(rObj_ChosenSite[1]):
         case "Webster":
-            print("Run Webster Function")
+            print("Running Webster Function")
             Webster.webster_WOTD(rObj_ChosenSite[0])
         case "Oxford":
             print("Run Oxford Function")
         case "Dictionary.com":
             print("Run Dictionary.com Function")
         case _: 
-            raise Exception("[Err] Default case was reached.")
+            print("[Main - Default case] Error with sitegenerator return  \n")
+            
+
 
 if __name__ == '__main__':
     main()
